81cc0ad60d1d5c918c950ef61050cc6c8e8864f5
jsolahr <51134556+jsolahr@users.noreply.github.com>
2019-07-20 10:14:28 -0500

    added functions in javascript file and updated css and index

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,39 +3,72 @@ $(document).ready(function() {
 var bananaText
 var graes;
 var apple; 
-var wins;
-var losses;
+var wins = 0;
+var losses = 0;
 var score = 0;
 var clickArray= [];
-var randomNumber = Math.floor((Math.random() * 90) + 30);
- 
+var randomNumber = Math.floor((Math.random() * 70) + 30);
+var bananaRandom = Math.floor((Math.random() * 11) +1); 
+var grapeRandom = Math.floor((Math.random() * 21) +1); 
+var appleRandom = Math.floor((Math.random() * 11) +1); 
 
 
 //Contractor to print Random Number //numberGenerater();
 function numberGenerater() {
     $("#random-number-box").text(randomNumber);
-};
-numberGenerater();
+}
 
+//Random # for Bananas 
+function bananaGenerator() {
+(bananaRandom);
+}
+bananaGenerator();
+console.log(bananaRandom);
+    
+//Random # for Grapes 
+function grapeGenerater() {
+(grapeRandom);
+}
+grapeGenerater();
+console.log(grapeRandom);
 
-//Onclick for Game Starts
-$("body").on("click", function(){
-
-});
-
-//Onclick for Apples 
-$("#appleClick").on("click", function(){
-});
+//Random # for Apples 
+function appleGenerater() {
+(appleRandom);
+}
+appleGenerater();
+console.log(appleRandom);
 
 //Onclick for Bananas
 $("#bananaClick").on("click", function(){
+    alert(bananaRandom);
     });
 //Onclick for Grapes
 $("#grapesClick").on("click", function(){
-    alert("grapes")
+    alert(grapeRandom)
     });
+//Onclick for Apples
+$("#appleClick").on("click", function(){
+    alert(appleRandom)
+    });
+
+//*****************************/MAIN GAME******************************//
+//Onclick for Game to Start
+$("body").on("click", function() {
+    numberGenerater();
+    $("#Score").text("Score: " + score);
+    $("#Wins").text("Wins: " + wins);
+    $("#Losses").text("Losses: " + losses);
 
 });
 
 
-  
+
+
+
+
+
+
+
+
+});

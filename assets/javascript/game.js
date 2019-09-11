90bc6b19733e223825846d73e235b939fbf0948f
jsolahr <51134556+jsolahr@users.noreply.github.com>
2019-09-11 13:19:29 -0500

    updating js file to remove 0 as a random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,30 +3,32 @@
 var wins = 0;
 var losses = 0;
 var score = 0;
-var bananaRandom = Math.floor(Math.random() * 13); 
-var grapeRandom = Math.floor(Math.random() * 13); 
-var appleRandom = Math.floor(Math.random() * 13);  
-var peachRandom = Math.floor(Math.random() * 13); 
+var bananaRandom = Math.floor(Math.random() * 13)+ 1;
+var grapeRandom = Math.floor(Math.random() * 13) +1; 
+var appleRandom = Math.floor(Math.random() * 13) +1; 
+var peachRandom = Math.floor(Math.random() * 13) +1; 
 var randomNumber = Math.floor((Math.random() * 80) + 40);
 
 //FUNCTIONS
 //=========================================================================================================
 function resetButtons (){
-    bananaRandom = Math.floor(Math.random() * 13); 
-    grapeRandom = Math.floor(Math.random() * 13); 
-    appleRandom = Math.floor(Math.random() * 13);  
-    peachRandom = Math.floor(Math.random() * 13);  
-}
+    bananaRandom = Math.floor(Math.random() * 13) +1; 
+    grapeRandom = Math.floor(Math.random() * 13) +1; 
+    appleRandom = Math.floor(Math.random() * 13) +1; 
+    peachRandom = Math.floor(Math.random() * 13) +1; 
+};
 
 $("#bananaClick").on("click", function()
 //Everytime user clicks the Banana a randomBanana # appears & adds to the score 
 {
     score = bananaRandom + score;
+    console.log(score);
     })
 $("#grapesClick").on("click", function()
 //Everytime user clicks the Banana a randomGrape # appears & adds to the score 
 {
     score = grapeRandom + score;
+    console.log(score);
     })
 //Everytime user clicks the Apple a randomApple # appears & adds to the score 
 $("#appleClick").on("click", function(){
@@ -35,7 +37,7 @@ $("#appleClick").on("click", function(){
     //Everytime user clicks the Peach a randomApple # appears & adds to the score 
 $("#peachClick").on("click", function(){
     score = peachRandom + score;
-    })
+    });
 
 //*****************************/MAIN GAME******************************//
 //Onclick for Game toto Start
